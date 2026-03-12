991aa9412bf0fef4efd5cd359f918641ec0daf12
trsr <trsr6750#@colorado.edu>
2026-03-12 10:17:48 -0600

    working w/ no UART

--- universal_servo_drive_f28p65x/sys_main.c
+++ universal_servo_drive_f28p65x/sys_main.c
@@ -46,7 +46,7 @@
 #include "board.h" // syscfg
 
 volatile SYSTEM_Vars_t systemVars;
-TelemetryUart_t UARTcomms;
+//TelemetryUart_t UARTcomms;
 #pragma DATA_SECTION(systemVars,"sys_data");
 
 #if defined(DAC128S_ENABLE)
@@ -261,10 +261,10 @@ void main(void)
     EINT;
 
     // initialize UART comms
-    TelemetryUart_init(&UARTcomms);
-    // Register a float variable pointer to be streamed
-    TelemetryUart_addChannel(&UARTcomms, &motorVars_M1.speed_Hz);
-    TelemetryUart_addChannel(&UARTcomms, &motorVars_M1.speedRef_Hz);
+    // TelemetryUart_init(&UARTcomms);
+    // // Register a float variable pointer to be streamed
+    // TelemetryUart_addChannel(&UARTcomms, &motorVars_M1.speed_Hz);
+    // TelemetryUart_addChannel(&UARTcomms, &motorVars_M1.speedRef_Hz);
 
     systemVars.powerRelayWaitTime_ms = POWER_RELAY_WAIT_TIME_ms;
 
